3f869881e1e8456169e04a71d9e01addc235f9f7
Svetoslav Kuzmanov <svetoslav@spotme.com>
2017-08-10 00:21:01 +0300

    Add php container

--- htdocs/example/index.php
+++ htdocs/example/index.php
@@ -5,7 +5,7 @@
   <title>Welcome to Example</title>
 </head>
 <body>
-  <h1>It works!</h1>
+  <h1><?php echo "It works!"; ?></h1>
   <h2>Welcome to Example</h2>
   <p>This is the default web page for this server.</p>
   <p>The web server software is running but no content has been added, yet.</p>
